1f6e3e97e3a785cc0f90645c47d66a61d9bcb735
siyam79 <siyamahmed3827@gmail.com>
2024-11-06 23:47:23 +0600

    Add removeDuplicates function to remove duplicate elements from an array

--- src/problem.ts
+++ src/problem.ts
@@ -6,7 +6,17 @@ function sumArray(num: number[]): number {
     return num.reduce((accumulator, num) => accumulator + num, 0)
 }
 const sumArrayResult = sumArray([1, 2, 3, 4, 5,])
-console.log(sumArrayResult);
+// console.log(sumArrayResult);
 
 //  end 
 
+// ------------->
+
+// problem number 2 :
+
+function removeDuplicates(num: number[]): number[] {
+    return Array.from(new Set(num))
+}
+
+const result = removeDuplicates([1, 2, 2, 3, 4, 4, 5])
+console.log(result);

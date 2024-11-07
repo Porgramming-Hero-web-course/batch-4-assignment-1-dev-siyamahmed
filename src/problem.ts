08542b89cecef1b7f27bfd27964094053ba4dc92
siyam79 <siyamahmed3827@gmail.com>
2024-11-07 19:42:02 +0600

    Added a generic function to retrieve object properties by key

--- src/problem.ts
+++ src/problem.ts
@@ -67,7 +67,23 @@
     // console.log(rectangleArea);
 
 
+    // ----------->
 
+    // Problem number 5 :
+
+
+    function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+        return obj[key]
+    }
+    const person = { name: "Alice", age: 30 };
+    const userInfo = getProperty(person, "name");
+    console.log(userInfo);
+
+
+
+    // ---------->
+
+   
 
 
 
